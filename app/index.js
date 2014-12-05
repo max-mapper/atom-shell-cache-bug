444704d77bda3ffebc66ec2606827156b14f3f07
Max Ogden <max@maxogden.com>
2014-12-04 16:21:09 -0800

    initial commit

---
+++ app/index.js
@@ -0,0 +1,29 @@
+var http = require('http')
+var path = require('path')
+var fs = require('fs')
+
+var app = require('app')  // Module to control application life.
+var BrowserWindow = require('browser-window')  // Module to create native browser window.
+
+var ecstatic = require('ecstatic')
+
+var clientDir = path.join(__dirname, '..', 'client')
+
+app.on('ready', function() {
+  // static file server
+  var server = http.createServer(ecstatic(clientDir))
+
+  server.listen(8765, function() {
+    var win = new BrowserWindow({
+      width: 500,
+      height: 500,
+      show : true,
+      'enable-larger-than-screen' : true,
+      'skip-taskbar' : true,
+      'use-content-size' : true
+    })
+
+    win.loadUrl("http://localhost:8765")
+    win.reloadIgnoringCache()
+  })
+})
